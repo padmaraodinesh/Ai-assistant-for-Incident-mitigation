6b5e77596634bba49df89afe19c018fdd3f10718
padmaraodinesh <62867356+padmaraodinesh@users.noreply.github.com>
2023-03-10 20:37:00 +0530

    Update openai_api.py
    added incident and mitigation related to cyber security

--- openai_api.py
+++ openai_api.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "key"
+openai.api_key = "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"
 
 def generate_mitigation(description):
     prompt = f'''
@@ -8,11 +8,11 @@ def generate_mitigation(description):
     Incident: Legit URL blocked
     Mitigation: check if url is legit or not -> delist the url -> perform RCA.
 
-    Incident: Network latency
-    Mitigation: Increased the bandwidth between the data centers and optimized the routing tables.
+    Incident: Delist legit IP address
+    Mitigation: check if Ip is legit or spammy -> check previous Ip traffic -> delist the IP from blocklist.
 
-    Incident: Database corruption
-    Mitigation: Restored the latest database backup and ran the database repair scripts to recover the data.
+    Incident: FN mail got into user inbox
+    Mitigation: Check if mail is FN or FP -> create a pattern rule to block that mail or campaign
 
     Incident: {description} 
     '''
@@ -22,10 +22,10 @@ def generate_mitigation(description):
         prompt=prompt,
         max_tokens=100,
         n=1,
-        stop='incident',
-        temperature=1,
+        stop='Incident',
+        temperature=0.7,
         frequency_penalty=0.5,
-        presence_penalty=0,
+        presence_penalty=0.5,
     )
     mitigation = response.choices[0].text.strip()
     return mitigation
