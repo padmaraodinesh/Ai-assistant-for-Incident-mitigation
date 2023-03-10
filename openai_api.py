18abfa82668cf55f52abe91fb44c56e8e4cafdfc
padmaraodinesh <62867356+padmaraodinesh@users.noreply.github.com>
2023-03-10 16:47:24 +0530

    Update openai_api.py

--- openai_api.py
+++ openai_api.py
@@ -1,16 +1,31 @@
 import openai
 
-openai.api_key = "api_key"
+openai.api_key = "key"
 
 def generate_mitigation(description):
-    prompt = f"give action plan to mitigate this security incident : {description}, in 200 words."
+    prompt = f'''
+   
+    Incident: Legit URL blocked
+    Mitigation: check if url is legit or not -> delist the url -> perform RCA.
+
+    Incident: Network latency
+    Mitigation: Increased the bandwidth between the data centers and optimized the routing tables.
+
+    Incident: Database corruption
+    Mitigation: Restored the latest database backup and ran the database repair scripts to recover the data.
+
+    Incident: {description} 
+    '''
+    
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt,
-        max_tokens=200,
+        max_tokens=100,
         n=1,
-        stop=None,
-        temperature=0.5,
+        stop='incident',
+        temperature=1,
+        frequency_penalty=0.5,
+        presence_penalty=0,
     )
     mitigation = response.choices[0].text.strip()
     return mitigation
